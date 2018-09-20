e591756a2c8e74c114223797483e165368cd1270
wengliming <1@11.com>
2018-09-20 19:15:20 +0800

    获取用户等级权限完成

--- angular/src/module/fenxitongji/fenxitongji-index/fenxitongji-index.js
+++ angular/src/module/fenxitongji/fenxitongji-index/fenxitongji-index.js
@@ -26,11 +26,13 @@
 					
 	                $scope.init = function () {
 	                	$scope.regionId = null;
+
 						// 区等级
-						$scope.regionGrade = 3;
+						$scope.regionGrade = 2;
                     	chartContainer();
 	                	getDataList();
 	                	regionTreeList();
+	                	getGrade();	                	
 	                }
 	                var $regionCode, $regionTreeContainer = $('#regionTreeContainer');
 	                var regionTree, reachTree, treeNode_find, treeNode_id;
@@ -41,7 +43,18 @@
 	                var regionTreeUrl = 'http://117.8.229.5:9000/information/v1/administrativeRegion/regionTree';
 	                
 	                
-	                
+	                //获取用户等级
+	                function getGrade(){
+	                	$http({
+	                        url: apiPrefix + '/v1/resumption/findCurrentUserGrade',
+	                        method: 'get'
+	                    }).success(function (res) {
+	                        //$scope.regionGrade = res.data;
+	                       // alert('11--'+$scope.regionGrade)
+	                    }).error(function (error) {
+	
+	                    })
+	                }
 	                
 
 	                // 开始时间
@@ -222,20 +235,23 @@
 	                function patrolMgrChart(data) {
 	                	this.chartInstance = [];
 	                	// 区等级
-	                	$scope.regionGrade = data[0] && data[0].regionGrade;
-
-	                	if ($scope.regionGrade == 3) {
-	                		this.chartInstance[0] = echarts.init($('#chart1')[0]);
+	                	//$scope.regionGrade = data[0] && data[0].regionGrade;
+						if ($scope.regionGrade == 2) {
+							this.chartInstance[0] = echarts.init($('#chart1')[0]);
 	                		this.chartInstance[1] = echarts.init($('#chart2')[0]);
 	                		this.chartInstance[2] = echarts.init($('#chart3')[0]);
-	                	} else if ($scope.regionGrade == 4) {
+						}else if ($scope.regionGrade == 3) {
 	                		$('#chart1').hide();
 	                		this.chartInstance[0] = echarts.init($('#chart2')[0]);
 	                		this.chartInstance[1] = echarts.init($('#chart3')[0]);
-	                	} else if ($scope.regionGrade == 5) {
+	                	} else if ($scope.regionGrade == 4) {
 	                		$('#chart1').hide();
 	                		$('#chart2').hide();
 	                		this.chartInstance[0] = echarts.init($('#chart3')[0]);
+	                	} else if ($scope.regionGrade == 5) {
+	                		$('#chart1').hide();
+	                		$('#chart2').hide();
+	                		$('#chart3').hide();
 	                	}
 	                	
 //	                	regionGrade == 3 ? 'rgb(85,205,246)' : (regionGrade == 4 ? 'rgb(246,231,126)' : 'rgb(140,151,203)')
@@ -316,7 +332,7 @@
 	                        var xAxis = [];
 	                        var series = {
 	                        	0: {
-	                        		title: '区级河长巡河情况',
+	                        		title: '村级河长巡河情况',
 	                        		series0: [],
 	                        		series1: [],
 	                        		series2: []
@@ -328,7 +344,7 @@
 	                        		series2: []
 	                        	},
 	                        	2: {
-	                        		title: '村级河长巡河情况',
+	                        		title: '区级河长巡河情况',
 	                        		series0: [],
 	                        		series1: [],
 	                        		series2: []
@@ -340,32 +356,31 @@
 	                        	
 	                            xAxis.push(item.regionName);	                           
 	                           	
-	                            if ($scope.regionGrade == 3) {
-		                            series[0].series0.push(item.countyHasPatrolNum);
-		                            series[0].series1.push(item.countyNeedPatrolNum);		                           
-		                            series[0].series2.push(item.countyPatrolRate);
+	                            if ($scope.regionGrade == 2) {
+		                            series[2].series0.push(item.countyHasPatrolNum);
+		                            series[2].series1.push(item.countyNeedPatrolNum);		                           
+		                            series[2].series2.push(item.countyPatrolRate);
 		                            
 		                            series[1].series0.push(item.townHasPatrolNum);
 		                            series[1].series1.push(item.townNeedPatrolNum);		                            
 		                            series[1].series2.push(item.townPatrolRate);
 		                            
-		                            series[2].series0.push(item.villageHasPatrolNum);
-		                            series[2].series1.push(item.villageNeedPatrolNum);		                            
-		                            series[2].series2.push(item.villagePatrolRate);
+		                            series[0].series0.push(item.villageHasPatrolNum);
+		                            series[0].series1.push(item.villageNeedPatrolNum);		                            
+		                            series[0].series2.push(item.villagePatrolRate);
 	                            }
 
-	                            
-	                            if ($scope.regionGrade == 4) {	                            	
-		                            series[0].series0.push(item.townHasPatrolNum);
-		                            series[0].series1.push(item.townNeedPatrolNum);		                           
-		                            series[0].series2.push(item.townPatrolRate);
+	                            if ($scope.regionGrade == 3) {	                            	
+		                            series[1].series0.push(item.townHasPatrolNum);
+		                            series[1].series1.push(item.townNeedPatrolNum);		                           
+		                            series[1].series2.push(item.townPatrolRate);
 		                            
-		                            series[1].series0.push(item.villageHasPatrolNum);
-		                            series[1].series1.push(item.villageNeedPatrolNum);	                            
-		                            series[1].series2.push(item.villagePatrolRate);
+		                            series[0].series0.push(item.villageHasPatrolNum);
+		                            series[0].series1.push(item.villageNeedPatrolNum);	                            
+		                            series[0].series2.push(item.villagePatrolRate);
 	                            }
 	                            
-	                            if ($scope.regionGrade == 5) {	                            	
+	                            if ($scope.regionGrade == 4) {	                            	
 		                            series[0].series0.push(item.villageHasPatrolNum);
 		                            series[0].series1.push(item.villageNeedPatrolNum);		                            
 		                            series[0].series2.push(item.villagePatrolRate);
@@ -376,11 +391,10 @@
 	                        for (var j = 0; j < this.chartInstance.length; j ++) {
 	                        	var chartData = JSON.parse(JSON.stringify(this.chartOption));
 	                        	chartData.xAxis.data = /*series[j].*/xAxis;
-	                        	chartData.series[0].data = series[j].series0;
-	                        	chartData.series[1].data = series[j].series1;
-	                        	chartData.series[2].data = series[j].series2;
+                        		chartData.series[0].data = series[j].series0;
+	                        	chartData.series[1].data = series[j].series1;	                        	
+	                        	chartData.series[2].data = series[j].series2;	                        	
 	                        	chartData.title.text = series[j].title;
-	                        	
 	                        	this.chartInstance[j].setOption(chartData);
 	                        }
 	                    }
