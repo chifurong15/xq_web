b3f7e6d7ca351fe84a29c0493cacfeeca41b90f0
wengliming <1@11.com>
2018-10-12 10:21:25 +0800

    提交地表水模块新增和查看

---
+++ angular/src/module/kaohepinggu/kaohepinggu-index/kaohepinggu-detail/kaohepinggu-detail.js
@@ -0,0 +1,76 @@
+(function(window, angular) {
+	'use strict';
+	angular
+		.module("app")
+		.controller(
+			'kaohepingfenViewCtrl',
+			[
+				'$localStorage',
+				'$scope',
+				'$location',
+				'$log',
+				'$q',
+				'$rootScope',
+				'$window',
+				'routeService',
+				'$http',
+				'$ajaxhttp',
+				'moduleService',
+				'globalParam',
+				function kaohepingfenViewCtrl($localStorage, $scope,
+						$location, $log, $q, $rootScope, $window,
+						routeService, $http, $ajaxhttp, moduleService, globalParam) {
+				
+					var apiPrefix = moduleService.getServiceUrl() + '/template';
+					
+					
+					$scope.init = function () {
+						var bulletin = globalParam.getter().bulletin || {};
+						if (!!getQueryString('id')) {
+							bulletin.id = getQueryString('id');
+						}
+						
+						// 编辑时获取原数据
+						if (bulletin.id) {
+							$location.search('id', bulletin.id);
+						}
+						getData(bulletin.id);
+					}
+					
+					//返回
+					$scope.goBack=function(){
+						history.back(-1);
+					}	
+					
+					
+					// 数据详情
+					function getData (id) {
+						$ajaxhttp.myhttp({
+							url: apiPrefix + '/v1/SurfaceWaterGrade/list',
+							method: 'get',
+							params: {
+								parentid: id
+							},
+							callBack: function (res) {
+								$scope.detailList = res.data;
+								console.log('shuju',res)
+//								var attandNamePart = res.data.attandUrl.split('_');
+//								$scope.bulletin = res.data;
+//								$scope.attandName = attandNamePart.splice(1, attandNamePart.length - 1).join('');
+							}
+						})
+					}
+					
+					// 获取url参数
+					function  getQueryString (params, url) {
+				        var url = url || location.href;
+				        var search = url.split('?')[1];
+				        if (search) {
+				            var n = new RegExp("(^|&)" + params + "=([^(&|#)]*)((&|#)|$)", "i"),
+				                r = search.match(n);
+				            return null != r ? r[2] : null
+				        }
+				        return null;
+				    }
+			} ]);
+})(window, angular);
